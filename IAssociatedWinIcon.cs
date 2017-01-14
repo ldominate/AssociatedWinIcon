36d82214f62de550052275a64b082b763958d8fc
Евгений <loki_jh@bk.ru>
2017-01-14 23:12:53 +1000

    Start init project

---
+++ IAssociatedWinIcon.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+
+namespace AssociatedWinIcon
+{
+	public interface IAssociatedWinIcon
+	{
+		Icon GetIcon(string extension);
+	}
+}
---
+++ IconInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace AssociatedWinIcon
+{
+	internal class IconInfo
+	{
+		public string Extension { get; set; }
+
+		public string IconPath { get; set; }
+		public Icon Icon { get; set; }
+	}
+}
